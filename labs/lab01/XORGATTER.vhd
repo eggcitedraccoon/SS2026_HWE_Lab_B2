10d2f1864a4b88b2737e8d182b59c1feaf341642
eggcitedraccoon <oleg.kelner@stud.hshl.de>
2026-05-07 12:30:05 +0200

    Clean up

--- labs/lab01/XORGATTER.vhd
+++ labs/lab01/XORGATTER.vhd
@@ -1,8 +1,8 @@
 entity XORGATTER is
-port (X,Y: in bit;
-Z:out bit);
+    port (X,Y: in bit;
+        Z    : out bit);
 end entity;
 architecture Data of XORGATTER is
 begin
-Z<= X xor Y;
+    Z<= X xor Y;
 end architecture;
