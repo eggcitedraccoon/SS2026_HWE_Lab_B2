5615b67f2bb8b495d35d7243e55d4f7812cf85b6
Oleg <31368965+eggcitedraccoon@users.noreply.github.com>
2026-05-09 17:38:26 +0200

    Fix architecture

--- labs/lab01/ORGATTER.vhd
+++ labs/lab01/ORGATTER.vhd
@@ -2,7 +2,7 @@ entity ORGATTER is
     port (X,Y: in bit;
         Z    : out bit);
 end entity;
-architecture Data of ANDGATTER is
+architecture Data of ORGATTER is
 begin
     Z<= X or Y;
 end architecture;
